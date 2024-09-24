e6e9329f8daf40a9f490657fc3db240cbf3bab86
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-24 13:26:35 +0700

    learn directory structure of laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+    <link rel="shortcut icon" href="img/laravel.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>ABOUT</h1>
+    </header>
+    <main>
+        <p>This Website Develop By {{ $developer }}</p>
+        <p>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+        </p>
+    </main>
+    <script src="js/script.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    $data['developer'] = 'Habibullah';
+    return view('about', $data);
 });
