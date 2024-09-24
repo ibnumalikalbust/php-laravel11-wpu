4867ff0c3a98276e7601945ee5d7f5566b9f5847
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-24 16:02:41 +0700

    learn blade templating engine

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,31 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/welcome', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    $data['title'] = 'HOME';
+    return view('home', $data);
+});
+
 Route::get('/about', function () {
+    $data['title'] = 'ABOUT';
     $data['developer'] = 'Habibullah';
     return view('about', $data);
 });
 
+Route::get('/contact', function () {
+    $data['title'] = 'CONTACT';
+    $data['facebook'] = 'https://facebook.com/ibnumalikalbust';
+    $data['instagram'] = 'https://instagram.com/ibnumalikalbust';
+    $data['twitter'] = 'https://twitter.com/ibnumalikalbust';
+    $data['tiktok'] = 'https://tiktok.com/@ibnumalikalbust';
+    return view('contact', $data);
+});
+
 Route::get('/blog', function () {
+    $data['title'] = 'BLOG';
     $data['articles'][1]['title'] = 'Fazzdev';
     $data['articles'][1]['paragraph1'] = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, maiores!';
     $data['articles'][1]['paragraph2'] = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate saepe repudiandae, ipsum fuga cum maxime officia placeat laboriosam modi alias?';
@@ -23,12 +35,4 @@ Route::get('/blog', function () {
     $data['articles'][2]['paragraph1'] = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, maiores!';
     $data['articles'][2]['paragraph2'] = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate saepe repudiandae, ipsum fuga cum maxime officia placeat laboriosam modi alias?';
     return view('blog', $data);
-});
-
-Route::get('/contact', function () {
-    $data['facebook'] = 'https://facebook.com/ibnumalikalbust';
-    $data['instagram'] = 'https://instagram.com/ibnumalikalbust';
-    $data['twitter'] = 'https://twitter.com/ibnumalikalbust';
-    $data['tiktok'] = 'https://tiktok.com/@ibnumalikalbust';
-    return view('contact', $data);
 });
