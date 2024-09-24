77bb79fc5d9dfd071459e335db137647caa8085b
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-25 05:45:48 +0700

    change indent style

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,16 +1,42 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <main class="my-3">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <?php foreach ($articles as $article) : ?>
-                    <h2>{{ $article['title'] }}</h2>
-                    <p>{{ $article['paragraph1'] }}</p>
-                    <p>{{ $article['paragraph2'] }}</p>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-        </div>
-    </main>
-</x-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<x-head>{{ $title }}</x-head>
+
+<body>
+	<x-navbar></x-navbar>
+	<header class="my-3">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<h1 class="text-center">{{ $title }}</h1>
+				</div>
+			</div>
+		</div>
+	</header>
+	<main class="my-3">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<?php foreach ($articles as $article) : ?>
+					<h2>{{ $article['title'] }}</h2>
+					<p>{{ $article['paragraph1'] }}</p>
+					<p>{{ $article['paragraph2'] }}</p>
+					<?php endforeach; ?>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer class="my-3">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<x-script></x-script>
+</body>
+
+</html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,43 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <main class="my-3">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <p class="text-center">
-                        <a class="btn btn-sm btn-primary" target="_blank" href="{{ $facebook }}">Facebook</a>
-                        <a class="btn btn-sm btn-primary" target="_blank" href="{{ $instagram }}">Instagram</a>
-                        <a class="btn btn-sm btn-primary" target="_blank" href="{{ $twitter }}">Twitter</a>
-                        <a class="btn btn-sm btn-primary" target="_blank" href="{{ $tiktok }}">Tiktok</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </main>
-</x-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<x-head>{{ $title }}</x-head>
+
+<body>
+	<x-navbar></x-navbar>
+	<header class="my-3">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<h1 class="text-center">{{ $title }}</h1>
+				</div>
+			</div>
+		</div>
+	</header>
+	<main class="my-3">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<p class="text-center">
+						<a class="btn btn-sm btn-primary" target="_blank" href="{{ $facebook }}">Facebook</a>
+						<a class="btn btn-sm btn-primary" target="_blank" href="{{ $instagram }}">Instagram</a>
+						<a class="btn btn-sm btn-primary" target="_blank" href="{{ $twitter }}">Twitter</a>
+						<a class="btn btn-sm btn-primary" target="_blank" href="{{ $tiktok }}">Tiktok</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer class="my-3">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<x-script></x-script>
+</body>
+
+</html>
