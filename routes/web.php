19d86060193b4f985d530d6f9174222198420c3a
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-26 17:40:45 +0700

    learn n+1 problem part 1

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/contact', function () {
 
 Route::get('/post', function () {
     $data['title'] = 'All Post';
-    $data['posts'] = Post::all();
+    $data['posts'] = Post::with(['authorDetail','categoryDetail'])->get();
     return view('posts', $data);
 });
 
@@ -43,12 +43,12 @@ Route::get('/post/{post:slug}', function (Post $post) {
 
 Route::get('/author/{author:slug}', function (Author $author) {
     $data['title'] = count($author->postsList) . ' Post By ' . $author->name;
-    $data['posts'] = $author->postsList;
+    $data['posts'] = $author->postsList->load('authorDetail','categoryDetail');
     return view('posts', $data);
 });
 
 Route::get('/category/{category:slug}', function (Category $category) {
     $data['title'] = count($category->postsList) . ' Post In ' . $category->name;
-    $data['posts'] = $category->postsList;
+    $data['posts'] = $category->postsList->load('authorDetail','categoryDetail');
     return view('posts', $data);
 });
